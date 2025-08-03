3e2a81c1ab502018298d9b3ff05b484cfdd0ddad
Devesh Prakash Singh <singh.dprakash@gmail.com>
2025-08-03 11:18:36 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,27 @@
 '''
 write a python code 
 
-to read for fhir server
+to read all the Patinet from fhir server who lives in Mysore
 
-fir.ly 
+fhir_base_url = 'https://server.fire.ly/Patient'
 '''
+
+import requests
+
+fhir_base_url = 'https://server.fire.ly/Patient'
+headers = {"Content-Type": "application/fhir+json"}
+
+patient_id = "a300358f-5942-4da2-aca0-9fefed48c4e5"  # Replace with your ID
+response = requests.get(f"{fhir_base_url}/{patient_id}")
+
+print(type(response))
+
+print(response)
+
+print("Status:", response.status_code)
+if response.status_code == 200:
+    print(f"Patinet with resource id : {patient_id} found \n")
+    print("Patient Resource:", response.json())
+else:
+    print(f"Patient with id :{patient_id} not found on the server")
+
